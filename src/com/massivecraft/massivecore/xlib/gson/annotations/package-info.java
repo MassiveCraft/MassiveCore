2e589a195e44d9cd14858634106503b6d28116aa
Olof Larsson <olof@sylt.nu>
2014-09-12 23:11:19 +0200

    GSON shade attempt fail

--- src/com/massivecraft/massivecore/xlib/gson/annotations/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package provides annotations that can be used with {@link com.massivecraft.massivecore.xlib.gson.Gson}.
- * 
- * @author Inderjeet Singh, Joel Leitch
- */
-package com.massivecraft.massivecore.xlib.gson.annotations;
