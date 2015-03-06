9649998352929ad4d48f2d2a2959a294ff8212cd
Olof Larsson <olof@sylt.nu>
2015-03-06 09:46:26 +0100

    Modify one of the suggest messages slightly.

--- src/com/massivecraft/massivecore/Lang.java
+++ src/com/massivecraft/massivecore/Lang.java
@@ -10,7 +10,7 @@ public class Lang
 	public static final String COMMAND_TOO_FEW_ARGS = "<b>Not enough command input. <i>You should use it like this:";
 	public static final String COMMAND_TOO_MANY_ARGS = "<b>Too much command input %s<b>.";
 	public static final String COMMAND_TOO_MANY_ARGS2 = "<i>You should use the command like this:";
-	public static final String COMMAND_NO_SUCH_SUB = "<b>The server couldn't find the command \"<c>%s<b>\".";
+	public static final String COMMAND_NO_SUCH_SUB = "<b>Couldn't find the command <c>%s";
 	public static final String COMMAND_SUGGEST_SUB = "<i>Maybe you could try %s";
 	public static final String COMMAND_GET_HELP = "<i>Use %s <i>to see commands.";
 }
