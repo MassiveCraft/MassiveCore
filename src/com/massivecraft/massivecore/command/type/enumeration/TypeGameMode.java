d76c2125b9a0d147e6b907c3f74b05a72f2bfa3a
Olof Larsson <olof@sylt.nu>
2015-12-15 11:32:47 +0100

    TypeGameMode now supports ordinals.

--- src/com/massivecraft/massivecore/command/type/enumeration/TypeGameMode.java
+++ src/com/massivecraft/massivecore/command/type/enumeration/TypeGameMode.java
@@ -1,6 +1,10 @@
 package com.massivecraft.massivecore.command.type.enumeration;
 
+import java.util.Set;
+
 import org.bukkit.GameMode;
+
+import com.massivecraft.massivecore.collections.MassiveSet;
 
 public class TypeGameMode extends TypeEnum<GameMode>
 {
@@ -13,6 +17,21 @@ public class TypeGameMode extends TypeEnum<GameMode>
 	public TypeGameMode()
 	{
 		super(GameMode.class);
+	}
+	
+	// -------------------------------------------- //
+	// OVERRIDE
+	// -------------------------------------------- //
+	
+	@Override
+	public Set<String> getIdsInner(GameMode value)
+	{
+		Set<String> ret = new MassiveSet<String>(super.getIdsInner(value));
+		
+		String id = String.valueOf(value.ordinal());
+		ret.add(id);
+		
+		return ret;
 	}
 
 }
