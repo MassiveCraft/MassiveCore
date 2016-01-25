6f73837b4fdb7d2d1feabbdda203c27a5031fb22
BuildTools <magnus@puj.dk>
2016-01-25 09:48:58 +0100

    Add PredicateVisibleTo and improve PredicateAnd

--- src/com/massivecraft/massivecore/PredicateAnd.java
+++ src/com/massivecraft/massivecore/PredicateAnd.java
@@ -1,10 +1,9 @@
 package com.massivecraft.massivecore;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
+
+import com.google.common.collect.ImmutableList;
 
 public class PredicateAnd<T> implements Predicate<T>
 {
@@ -12,25 +11,25 @@ public class PredicateAnd<T> implements Predicate<T>
 	// INSTANCE & CONSTRUCT
 	// -------------------------------------------- //
 	@SafeVarargs
-	public static <T> PredicateAnd<T> get(Predicate<T>... predicates) { return new PredicateAnd<T>(predicates); }
+	public static <T> PredicateAnd<T> get(Predicate<? super T>... predicates) { return new PredicateAnd<T>(predicates); }
 	@SafeVarargs
-	public PredicateAnd(Predicate<T>... predicates)
+	public PredicateAnd(Predicate<? super T>... predicates)
 	{
-		this(Arrays.asList(predicates));
+		this(ImmutableList.copyOf(predicates));
 	}
 	
-	public static <T> PredicateAnd<T> get(Collection<Predicate<T>> predicates) { return new PredicateAnd<T>(predicates); }
-	public PredicateAnd(Collection<Predicate<T>> predicates)
+	public static <T> PredicateAnd<T> get(Collection<Predicate<? super T>> predicates) { return new PredicateAnd<T>(predicates); }
+	public PredicateAnd(Collection<Predicate<? super T>> predicates)
 	{
-		this.predicates = Collections.unmodifiableList(new ArrayList<Predicate<T>>(predicates));
+		this.predicates = ImmutableList.copyOf(predicates);
 	}
 	
 	// -------------------------------------------- //
 	// FIELDS
 	// -------------------------------------------- //
 	
-	private final List<Predicate<T>> predicates;
-	public List<Predicate<T>> getPredicates() { return this.predicates; }
+	private final List<Predicate<? super T>> predicates;
+	public List<Predicate<? super T>> getPredicates() { return this.predicates; }
 	
 	// -------------------------------------------- //
 	// OVERRIDE
@@ -39,7 +38,7 @@ public class PredicateAnd<T> implements Predicate<T>
 	@Override
 	public boolean apply(T type)
 	{
-		for (Predicate<T> predicate : this.getPredicates())
+		for (Predicate<? super T> predicate : this.getPredicates())
 		{
 			if (!predicate.apply(type)) return false;
 		}
