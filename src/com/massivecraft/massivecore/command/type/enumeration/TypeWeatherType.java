9e193ef75fe80fc5d99607e1dcaa83baace62d7a
Olof Larsson <olof@sylt.nu>
2016-06-02 11:30:23 +0200

    TypeWeatherType - Aliases and Colorization

--- src/com/massivecraft/massivecore/command/type/enumeration/TypeWeatherType.java
+++ src/com/massivecraft/massivecore/command/type/enumeration/TypeWeatherType.java
@@ -2,7 +2,9 @@ package com.massivecraft.massivecore.command.type.enumeration;
 
 import java.util.Set;
 
+import org.bukkit.ChatColor;
 import org.bukkit.WeatherType;
+import org.bukkit.command.CommandSender;
 
 import com.massivecraft.massivecore.collections.MassiveSet;
 
@@ -24,14 +26,42 @@ public class TypeWeatherType extends TypeEnum<WeatherType>
 	// -------------------------------------------- //
 	
 	@Override
+	public ChatColor getVisualColor(WeatherType value, CommandSender sender)
+	{
+		if (value == null) return ChatColor.GRAY;
+		if (value == WeatherType.CLEAR) return ChatColor.GREEN;
+		return ChatColor.RED;
+	}
+	
+	@Override
+	public String getNameInner(WeatherType value)
+	{
+		switch (value)
+		{
+			case DOWNFALL: return "Rain";
+			case CLEAR: return "Sun";
+		}
+		throw new RuntimeException();
+	}
+	
+	@Override
 	public Set<String> getNamesInner(WeatherType value)
 	{
-		Set<String> ret = new MassiveSet<String>(super.getNamesInner(value));
+		Set<String> ret = new MassiveSet<String>();
 		
-		if (value == WeatherType.DOWNFALL)
+		switch (value)
 		{
-			ret.add("Rain");
+			case DOWNFALL:
+				ret.add("Rain");
+				ret.add("Storm");
+			break;
+			case CLEAR:
+				ret.add("Sun");
+				ret.add("Sky");
+			break;
 		}
+		
+		ret.addAll(super.getNamesInner(value));
 		
 		return ret;
 	}
