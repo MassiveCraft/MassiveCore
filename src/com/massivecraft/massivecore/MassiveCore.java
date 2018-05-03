11add07090b071739f586325329cb77607c6ce11
Olof Larsson <olof@sylt.nu>
2018-05-03 10:22:06 +0200

    Remove Uneccessary debug changed call

--- src/com/massivecraft/massivecore/MassiveCore.java
+++ src/com/massivecraft/massivecore/MassiveCore.java
@@ -259,14 +259,6 @@ public class MassiveCore extends MassivePlugin
 		// Delete Files (at once and additionally after all plugins loaded)
 		MassiveCoreTaskDeleteFiles.get().run();
 		Bukkit.getScheduler().scheduleSyncDelayedTask(this, MassiveCoreTaskDeleteFiles.get());
-
-		// Uneccessary call of .changed() for debug
-		Bukkit.getScheduler().runTaskLater(this, new Runnable() {
-			@Override
-			public void run() {
-				MassiveCoreMConf.get().changed();
-			}
-		}, 200L);
 	}
 
 	@Override
